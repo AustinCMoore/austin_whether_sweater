cf32c8de7d91076efdebdb98410f5707eb826f19
Austin Moore <austinchristianmoore@gmail.com>
2022-04-24 19:43:49 -0400

    Fix: API key issues

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.create!(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+    user = User.create!(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], api_key: SecureRandom.base64(10))
     render json: UsersSerializer.new(user)
+    #sad path: email duplicate
+    #sad path: email empty
+
+    #sad path: passwords dont match
+    #sad path: password empty
+    #sad path: password confirmation empty
   end
 end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users request' do
   it "Creates a user with proper information" do
+    user_count = User.all.count
     post "/api/v1/users", :params => {
                                       :email => "whatever@example.com",
                                       :password => "password",
@@ -20,10 +21,15 @@ RSpec.describe 'Users request' do
     expect(user[:data]).to have_key(:attributes)
     expect(user[:data][:attributes]).to be_a(Hash)
 
+    expect(user[:data][:attributes]).to_not have_key(:password)
+    expect(user[:data][:attributes]).to_not have_key(:password_confirmation)
+
     expect(user[:data][:attributes]).to have_key(:email)
     expect(user[:data][:attributes][:email]).to eq("whatever@example.com")
 
     expect(user[:data][:attributes]).to have_key(:api_key)
     expect(user[:data][:attributes][:api_key]).to be_a(String)
+
+    expect(User.all.count).to eq(user_count += 1)
   end
 end
