89b5c2838ccf0a9c8c9361934d9b4aa4e293862a
Austin Moore <austinchristianmoore@gmail.com>
2022-04-24 17:57:33 -0400

    Feature: Create happy path

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.create!(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+    render json: UsersSerializer.new(user)
+  end
+end
